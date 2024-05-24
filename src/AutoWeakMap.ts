7b629f584fefb310f3a907847cc95112dd5bbc73
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-05-24 09:05:22 +0200

    version 0.5.18

--- src/AutoWeakMap.ts
+++ src/AutoWeakMap.ts
@@ -7,18 +7,6 @@ export class AutoWeakMap {
         createValue: (...key: [C, ...K]) => V
     ): ((...key: [C, ...K]) => V) => {
 
-        // this commented version has a memory leak
-        // type AutoMapFunc = (key: K) => V;
-
-        // const weakMap: AutoWeakMap<C, AutoMapFunc> = new AutoWeakMap(
-        //     (context: C): AutoMapFunc => {            
-        //         const autoMap = AutoMap.create<K, V>(
-        //             (...key: K) => createValue(context, ...key)
-        //         );
-        //         return (key: K): V => autoMap(...key);
-        //     }
-        // );
-
         const week = new WeakMap<C, AutoMap<K, V>>();
 
         return (...key: [C, ...K]): V => {
