e68dc6986a9b63c2c6b451a585702b261d674bd2
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-05-24 19:09:32 +0200

    version 0.5.24

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,5 +1,3 @@
-/** @jsx createElement */
-import { createElement } from 'react';
 import { makeObservable, observable } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
