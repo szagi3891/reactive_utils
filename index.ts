0b42955297f21e0ef1181b0559f181910e687070
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-05-24 19:36:35 +0200

    version 0.5.25

--- index.ts
+++ index.ts
@@ -12,10 +12,9 @@ export { getValueCache } from './src/getValueCache.ts';
 export { Result } from './src/Result.ts';
 export type { ResultOk, ResultError } from './src/Result.ts';
 export { Semaphore } from './src/Semaphore.ts';
-export { TestComponent } from './src/TestComponent.tsx';
 
 
 
-//...
+//... without jsx
 
 
