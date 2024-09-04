bcd87fbe860e2e507ad63c578e92c35b9f9f8a7d
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-09-04 20:48:40 +0200

    version 0.5.38

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ export { Resource } from './src/Resource.ts';
 export type { ResourceResult } from './src/Resource.ts';
 export { PromiseBox } from './src/PromiseBox.ts';
 export { AutoMap } from './src/AutoMap/AutoMap.ts';
-export { autoMapKeyAsString } from './src/AutoMap/PrimitiveType.ts';
+export { autoMapKeyAsString, PrimitiveType, reduceComplexSymbol } from './src/AutoMap/PrimitiveType.ts';
 export { AutoWeakMap, autoWeakMapKey } from './src/AutoMap/AutoWeakMap.ts';
 export { registerNetworRequest, whenIdle, ssrReactLazy, ResourceSSR } from './src/Idle.ts';
 export { EventEmitter, ValueEmitter } from './src/EventEmitter.ts';
@@ -13,7 +13,6 @@ export { getValueCache } from './src/getValueCache.ts';
 export { Result } from './src/Result.ts';
 export type { ResultOk, ResultError } from './src/Result.ts';
 export { Semaphore } from './src/Semaphore.ts';
-export { PrimitiveType, reduceComplexSymbol } from './src/AutoMap/PrimitiveType.ts';
 
 
 //... without jsx
