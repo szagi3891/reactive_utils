8eab631f8836391bdaaf7f2525e6c6613c7a2993
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-09-15 21:20:16 +0200

    version 0.5.48

--- index.ts
+++ index.ts
@@ -17,6 +17,7 @@ export { Semaphore } from './src/Semaphore.ts';
 export { AutoId } from './src/AutoId.ts';
 export { AsyncQuery } from './src/AsyncQuery.ts';
 export { AsyncWebSocket } from './src/AsyncWebSocket.ts';
+export { Defer } from './src/Defer.ts';
 
 //... without jsx
 
