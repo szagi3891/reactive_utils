7ca542311f02634c90a3fd2840c95cd6be019ae4
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-10-23 15:50:33 +0200

    version 0.5.103

--- src/AsyncQuery.test.ts
+++ src/AsyncQuery.test.ts
@@ -1,6 +1,18 @@
-import { AsyncQuery } from "./AsyncQuery.ts";
+import { AsyncQuery, AsyncQueryIterator } from "./AsyncQuery.ts";
 import { expect } from "jsr:@std/expect";
 import { timeout } from "./timeout.ts";
+
+const subscribeTo = (query: AsyncQuery<number>, result: Array<number>): AsyncQueryIterator<number> => {
+    const iterator = query.subscribe();
+
+    (async () => {
+        for await (const message of iterator) {
+            result.push(message);
+        }
+    })();
+    
+    return iterator;
+}
 
 Deno.test('basic', async () => {
     const list: Array<number> = [];
@@ -18,11 +30,12 @@ Deno.test('basic', async () => {
     expect(abort).toBe(false);
     expect(list).toEqual([]);
 
-    (async () => {
-        for await (const message of query.subscribe()) {
-            list.push(message);
-        }
-    })();
+    const _sub1 = subscribeTo(query, list);
+    // (async () => {
+    //     for await (const message of query.subscribe()) {
+    //         list.push(message);
+    //     }
+    // })();
 
     await timeout(0);
     expect(abort).toBe(false);
@@ -42,6 +55,84 @@ Deno.test('basic', async () => {
 
     expect(abort).toBe(true);
     expect(list).toEqual([3, 4, 5]);
+});
+
+Deno.test('multi', async () => {
+    const list1: Array<number> = [];
+    const list2: Array<number> = [];
+
+    const query = new AsyncQuery<number>();
+
+    const sub1 = subscribeTo(query, list1);
+    const sub2 = subscribeTo(query, list2);
+
+    expect(list1).toEqual([]);
+    expect(list2).toEqual([]);
+
+    query.push(1);
+    await timeout(0);
+
+    expect(list1).toEqual([1]);
+    expect(list2).toEqual([]);
+
+    query.push(2);
+    await timeout(0);
+
+    expect(list1).toEqual([1]);
+    expect(list2).toEqual([2]);
+
+    query.push(3);
+    await timeout(0);
+
+    expect(list1).toEqual([1, 3]);
+    expect(list2).toEqual([2]);
+
+    query.push(4);
+    await timeout(0);
+
+    expect(list1).toEqual([1, 3]);
+    expect(list2).toEqual([2, 4]);
+
+    sub2.unsubscribe();
+
+    query.push(5);
+    await timeout(0);
+
+    expect(list1).toEqual([1, 3, 5]);
+    expect(list2).toEqual([2, 4]);
+
+    query.push(6);
+    await timeout(0);
+
+    expect(list1).toEqual([1, 3, 5, 6]);
+    expect(list2).toEqual([2, 4]);
+
+    query.push(7);
+    await timeout(0);
+
+    expect(list1).toEqual([1, 3, 5, 6, 7]);
+    expect(list2).toEqual([2, 4]);
+
+    sub1.unsubscribe();
+
+    query.push(8);
+    query.push(9);
+    query.push(10);
+
+    expect(list1).toEqual([1, 3, 5, 6, 7]);
+    expect(list2).toEqual([2, 4]);
+
+    const list3: Array<number> = [];
+    const sub3 = subscribeTo(query, list3);
+
+    await timeout(0);
+    expect(list3).toEqual([8, 9, 10]);
+
+    sub3.unsubscribe();
+    query.push(11);
+    await timeout(0);
+
+    expect(list3).toEqual([8, 9, 10]);
 });
 
 
