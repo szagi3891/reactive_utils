d69a9b791b903c0976be3506539453389909c209
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-10-25 12:48:48 +0200

    version 0.5.110

--- src/ValueList.test.ts
+++ src/ValueList.test.ts
@@ -1,7 +1,34 @@
+import { expect } from "jsr:@std/expect";
+import { ValueList } from "./ValueList.ts";
+
+type ID = {
+    eventID: string,
+    subID: string,
+};
+
+type Value = string;
 
 Deno.test('basic', () => {
 
+    const list = new ValueList<ID, Value>();
+    
+    const replica = new ValueList<ID, Value>();
+    list.onChange(data => {
+        replica.bulkUpdate(data);
+    });
 
 
+    expect(list.ids).toEqual([]);
+    expect(replica.ids).toEqual([]);
+
+    list.set({ eventID: 'a', subID: 'b'}, 'rrr');
+
+    expect(list.ids).toEqual([{ eventID: 'a', subID: 'b'}]);
+    expect(replica.ids).toEqual([{ eventID: 'a', subID: 'b'}]);
+
+    list.set({ eventID: 'c', subID: 'd'}, 'ccc');
+
+    expect(list.ids).toEqual([{ eventID: 'a', subID: 'b'}, { eventID: 'c', subID: 'd'}]);
+    expect(replica.ids).toEqual([{ eventID: 'a', subID: 'b'}, { eventID: 'c', subID: 'd'}]);
 });
 
--- src/ValueList.ts
+++ src/ValueList.ts
@@ -1,66 +1,75 @@
-import { action } from 'mobx';
+import { runInAction } from 'mobx';
 import { assertNever } from './assertNever.ts';
 import { Result } from "./Result.ts";
 import { ValueUnsafe } from "./ValueUnsafe.ts";
 import type { JSONValue } from "../index.ts";
 import { MapJson } from "./AutoMap/MapJson.ts";
+import { EventEmitter } from "./EventEmitter.ts";
 
 //Najlepiej żeby klucz pozwalał na posortowanie listy. Po to, żeby wyrenderować szkielet, bez zagłębiania się w 
 //jeśli klucz będzie bardziej złożony, to wtedy można wykorzystać AutoMap w celu nadania porównywalności po referencji, a potem sortować
 
+type ChangeType<ID extends JSONValue, M> = {
+    type: 'set',
+    id: ID,
+    value: M
+} | {
+    type: 'delete',
+    id: ID
+};
+
 export class ValueList<ID extends JSONValue, M> {
     private readonly listVal: ValueUnsafe<Array<ID>>;
     private readonly modelVal: MapJson<ID, ValueUnsafe<M>>;
+    private readonly events: EventEmitter<Array<ChangeType<ID, M>>>;
 
     constructor() {
         this.listVal = new ValueUnsafe([]);
         this.modelVal = new MapJson();
+        this.events = new EventEmitter();
     }
 
-    set(key: ID, value: M) {
-        const prevValue = this.modelVal.get(key);
+    private setInner(id: ID, value: M) {
+        const prevValue = this.modelVal.get(id);
 
         if (prevValue === undefined) {
             const newValue = new ValueUnsafe<M>(value);
-            //TODO - dodać id do listy
-            this.modelVal.set(key, newValue);
+            this.modelVal.set(id, newValue);
 
-            //TODO - dodać emitowanie zdarzenia
+            this.listVal.value.push(id);
+            this.listVal.atom.reportChanged();
 
             return;
         }
 
         prevValue.value = value;
         prevValue.atom.reportChanged();
-
-        //TODO - dodać emitowanie zdarzenia
     }
 
-    delete(key: ID) {
-        this.modelVal.delete(key);
+    private deleteInner(id: ID) {
+        this.modelVal.delete(id);
 
-        if (this.listVal.value.includes(key)) {
-            this.listVal.value = this.listVal.value.filter(item => item !== key);
+        if (this.listVal.value.includes(id)) {
+            this.listVal.value = this.listVal.value.filter(item => item !== id);
             this.listVal.atom.reportChanged();
         }
     }
 
-    onChange(callback: (data: Array<{ type: 'set', id: ID, value: M } | { type: 'delete', id: ID }>) => void): (() => void) {
-        
-        throw Error('aaa');
+    onChange(callback: (data: Array<ChangeType<ID, M>>) => void): (() => void) {
+        return this.events.on(callback);
     }
 
     //Metoda która pozwoli na ustawienie wielu rekordów na raz lub skasowania
-    bulkUpdate(data: Array<{ type: 'set', id: ID, value: M } | { type: 'delete', id: ID }>) {
-        action(() => {
+    bulkUpdate(data: Array<ChangeType<ID, M>>) {
+        runInAction(() => {
             for (const record of data) {
                 switch (record.type) {
                     case 'set': {
-                        this.set(record.id, record.value);
+                        this.setInner(record.id, record.value);
                         break;
                     }
                     case 'delete': {
-                        this.delete(record.id);
+                        this.deleteInner(record.id);
                         break;
                     }
                     default: {
@@ -68,14 +77,31 @@ export class ValueList<ID extends JSONValue, M> {
                     }
                 }
             }
+
+            this.events.trigger(data);
         });
+    }
+
+    public set(id: ID, value: M) {
+        this.bulkUpdate([{
+            type: 'set',
+            id,
+            value,
+        }]);
+    }
+
+    public delete(id: ID) {
+        this.bulkUpdate([{
+            type: 'delete',
+            id,
+        }]);
     }
 
     //Te dwie metody, przeznaczone byłyby dla widoku
     //Id mógłby być tak skonstruowany, żeby dało się po nim sortować
 
     //getter pozwalający pobrać listę idków
-    list(): Array<ID> {
+    get ids(): Array<ID> {
         this.listVal.atom.reportObserved();
         return this.listVal.value;
     }
