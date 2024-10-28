f300e14de3f3b9a978dd777c6ed07de0e60a3cd9
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-10-28 08:21:32 +0100

    version 0.5.128

--- src/websocket/SocketRouter.ts
+++ src/websocket/SocketRouter.ts
@@ -1,62 +1,58 @@
 import { JSONValue } from "../Json.ts";
 import { z } from 'zod';
 
-const valueList = <
+export class DefValueList<
     R extends JSONValue,
     ID extends JSONValue,
     M extends JSONValue
->(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>) => {
-return {
-    resourceId,
-    resp: z.array(z.union([
-        z.object({
-            type: z.literal('set'),
-            id: idType,
-            model: modelType,
-        }),
-        z.object({
-            type: z.literal('delete'),
-            id: idType,
-        }),
-    ])),
-};
+> {
+
+    private readonly resourceId: z.ZodType<R, z.ZodTypeDef, R>;
+    private readonly resp;
+
+    //W razie problemów, tą klasę można zamienić na funkcję, po to żeby nie musieć definioać tych skoplikowanych typów zod
+
+    constructor(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>) {
+        this.resourceId = resourceId;
+
+        this.resp = z.array(z.union([
+            z.object({
+                type: z.literal('set'),
+                id: idType,
+                model: modelType,
+            }),
+            z.object({
+                type: z.literal('delete'),
+                id: idType,
+            }),
+        ]));
+    }
 }
 
-const value = <
+export class DefValue<
     R extends JSONValue,
     M extends JSONValue,
->(resourceId: z.ZodType<R>, modelType: z.ZodType<M>) => {
-    return {
-        resourceId,
-        resp: modelType
+> {
+    constructor(
+        private readonly resourceId: z.ZodType<R>,
+        private readonly resp: z.ZodType<M>
+    ) {
     }
-};
-
-export type ValueListReturnType<R extends JSONValue, ID extends JSONValue, M extends JSONValue> = ReturnType<
-    typeof valueList<R, ID, M>
->;
-
-// Typ dla funkcji value
-export type ValueReturnType<R extends JSONValue, M extends JSONValue> = ReturnType<
-    typeof value<R, M>
->;
+}
 
 export class SocketRouter {
     public static valueList = <
         R extends JSONValue,
         ID extends JSONValue,
         M extends JSONValue
-    >(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>): ValueListReturnType<R, ID, M> => {
-        return valueList(resourceId, idType, modelType);
+    >(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>): DefValueList<R, ID, M> => {
+        return new DefValueList(resourceId, idType, modelType);
     }
 
     public static value = <
         R extends JSONValue,
         M extends JSONValue,
-    >(resourceId: z.ZodType<R>, modelType: z.ZodType<M>): ValueReturnType<R, M> => {
-        return {
-            resourceId,
-            resp: modelType
-        }
+    >(resourceId: z.ZodType<R>, modelType: z.ZodType<M>): DefValue<R, M> => {
+        return new DefValue(resourceId, modelType);
     };
 }
