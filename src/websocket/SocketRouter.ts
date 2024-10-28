bb34eb4d7ed88053756904b63e067e501fd2fa6f
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-10-28 07:27:14 +0100

    version 0.5.127

--- src/websocket/SocketRouter.ts
+++ src/websocket/SocketRouter.ts
@@ -1,36 +1,62 @@
 import { JSONValue } from "../Json.ts";
 import { z } from 'zod';
 
+const valueList = <
+    R extends JSONValue,
+    ID extends JSONValue,
+    M extends JSONValue
+>(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>) => {
+return {
+    resourceId,
+    resp: z.array(z.union([
+        z.object({
+            type: z.literal('set'),
+            id: idType,
+            model: modelType,
+        }),
+        z.object({
+            type: z.literal('delete'),
+            id: idType,
+        }),
+    ])),
+};
+}
+
+const value = <
+    R extends JSONValue,
+    M extends JSONValue,
+>(resourceId: z.ZodType<R>, modelType: z.ZodType<M>) => {
+    return {
+        resourceId,
+        resp: modelType
+    }
+};
+
+export type ValueListReturnType<R extends JSONValue, ID extends JSONValue, M extends JSONValue> = ReturnType<
+    typeof valueList<R, ID, M>
+>;
+
+// Typ dla funkcji value
+export type ValueReturnType<R extends JSONValue, M extends JSONValue> = ReturnType<
+    typeof value<R, M>
+>;
+
 export class SocketRouter {
     public static valueList = <
         R extends JSONValue,
         ID extends JSONValue,
         M extends JSONValue
-    >(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>) => {
-        return {
-            resourceId,
-            resp: z.array(z.union([
-                z.object({
-                    type: z.literal('set'),
-                    id: idType,
-                    model: modelType,
-                }),
-                z.object({
-                    type: z.literal('delete'),
-                    id: idType,
-                }),
-            ])),
-        };
+    >(resourceId: z.ZodType<R>, idType: z.ZodType<ID>, modelType: z.ZodType<M>): ValueListReturnType<R, ID, M> => {
+        return valueList(resourceId, idType, modelType);
     }
 
     public static value = <
         R extends JSONValue,
         M extends JSONValue,
-    >(resourceId: z.ZodType<R>, modelType: z.ZodType<M>) => {
+    >(resourceId: z.ZodType<R>, modelType: z.ZodType<M>): ValueReturnType<R, M> => {
         return {
             resourceId,
             resp: modelType
         }
     };
 }
-
