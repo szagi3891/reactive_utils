463b319799623fac06f2cb6ec06c0f5e7fd717e1
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-11-29 07:19:07 +0100

    version 0.5.153

--- index.ts
+++ index.ts
@@ -17,6 +17,7 @@ export { Semaphore } from './src/Semaphore.ts';
 export { AutoId } from './src/AutoId.ts';
 export { AsyncQuery, AsyncQueryIterator } from './src/AsyncQuery.ts';
 export { AsyncWebSocket } from './src/AsyncWebSocket.ts';
+export { WebsocketStream } from './src/websocket/client/WebsocketStream.ts';
 export { Defer } from './src/Defer.ts';
 export { jsonParseRaw, jsonParse, stringifySort, JSONValueZod } from './src/Json.ts';
 export type { JSONValue } from './src/Json.ts';
