0d01875837b2b3184a16413596ca3adbd713cf1c
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-01-05 10:35:12 +0100

    version 0.5.182

--- src/checkByZod.ts
+++ src/checkByZod.ts
@@ -39,8 +39,8 @@ export class CheckByZodError {
 
         return new CheckByZodError(
             [
+                message,
                 ...description,
-                message
             ],
             this.errors,
             this.data
