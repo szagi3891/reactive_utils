43a58a0ddfc2da39a06464fd653c29de362f514a
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-02-07 06:21:07 +0100

    version 0.5.190

---
+++ src/ComputedStruct.test.ts
@@ -0,0 +1,37 @@
+import { Value } from '@reactive/utils';
+import { ComputedStruct } from './ComputedStruct.ts';
+import { autorun } from 'mobx';
+import { expect } from "jsr:@std/expect";
+
+Deno.test('ComputedStruct', () => {
+    const sourceValue = new Value<Array<{name: string}>>([]);
+
+    const computed = ComputedStruct.initStructural(() => {
+        return sourceValue.getValue();
+    });
+
+    let counter: number = 0;
+
+    const dispose = autorun(() => {
+        counter++;
+
+        computed.getValue();
+    });
+
+    expect(counter).toBe(1);
+    sourceValue.setValue([]);
+    expect(counter).toBe(1);
+
+    sourceValue.setValue([{ name: 'aaa' }]);
+    expect(counter).toBe(2);
+    sourceValue.setValue([{ name: 'aaa' }]);
+    expect(counter).toBe(2);
+
+    sourceValue.setValue([{ name: 'aaa' }, { name: 'bbb'}]);
+    expect(counter).toBe(3);
+    sourceValue.setValue([{ name: 'aaa' }, { name: 'bbb'}]);
+    expect(counter).toBe(3);
+
+    dispose();
+});
+
---
+++ src/ComputedStruct.ts
@@ -0,0 +1,42 @@
+import { computed, comparer, type IComputedValue } from "mobx";
+
+export const compareArray = <T>(list1: Array<T>, list2: Array<T>): boolean => {
+    if (list1.length !== list2.length) {
+        return false;
+    }
+
+    for (let index = 0; index < list1.length; index++) {
+        if (list1[index] !== list2[index]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+export class ComputedStruct<T> {
+
+    private readonly computedValue: IComputedValue<T>;
+
+    private constructor(
+        value: () => T,
+        equals: (a: T, b: T) => boolean
+    ) {
+        this.computedValue = computed(() => {
+            return value();
+        }, {
+            equals,
+        });
+    }
+
+    static initStructural<T>(value: () => T): ComputedStruct<T> {
+        return new ComputedStruct(value, comparer.structural);
+    }
+
+    static initArrays<T>(value: () => Array<T>): ComputedStruct<Array<T>> {
+        return new ComputedStruct(value, compareArray);
+    }
+
+    getValue(): T {
+        return this.computedValue.get();
+    }
+}
