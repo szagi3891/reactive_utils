4766d39003607f7fff6bc04a65d49a46c5442f2e
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-03-03 08:16:42 +0100

    version 0.5.196

--- src/Result.ts
+++ src/Result.ts
@@ -1,27 +1,59 @@
-export interface ResultError<E> {
-    readonly type: 'error';
-    readonly error: E
+export class ResultError<E> {
+    constructor(
+        public readonly type: 'error',
+        public readonly error: E
+    ) {}
+
+    map(_mapFun: (value: unknown) => unknown): ResultError<E> {
+        return this;
+    }
 }
 
-export interface ResultOk<T> {
-    readonly type: 'ok';
-    readonly data: T;
+export class ResultOk<T> {
+    constructor(
+        public readonly type: 'ok',
+        public readonly data: T,
+    ) {}
+
+    map<T2>(mapData: (value: T) => T2): ResultOk<T2> {
+        return new ResultOk(
+            'ok',
+            mapData(this.data)
+        );
+    }
 }
 
 export type Result<T, E> = ResultOk<T> | ResultError<E>;
 
 export const Result = {
     ok: <T>(data: T): Result<T, never> => {
-        return {
-            type: 'ok',
+        return new ResultOk(
+            'ok',
             data
-        };
+        );
     },
   
     error: <E>(error: E): Result<never, E> => {
-        return {
-            type: 'error',
+        return new ResultError(
+            'error',
             error
-        }
+        );
     }
 }
+
+
+//TODO - sprawdzić mapowanie result<T1, E> na Result<T2, E>
+
+// const fun1 = (): Result<number, number> => {
+//     throw Error('TODO');
+// };
+
+// const rrr = (): Result<string, number> => {
+
+//     //: Result<string, number>
+//     const ggg = fun1().map(() => '');
+
+//     return ggg;
+// }
+
+// console.info(rrr);
