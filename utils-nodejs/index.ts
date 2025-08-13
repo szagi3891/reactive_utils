2d341fbbf80e7c6dc84637ee6eb3cd3763df6d8a
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-08-13 08:51:54 +0200

    version 0.1.5

--- utils-nodejs/index.ts
+++ utils-nodejs/index.ts
@@ -1,5 +1,5 @@
 
-export { exec } from './src/exec.ts';
+export { exec, execSsh } from './src/exec.ts';
 export { execAndGet } from './src/execAndGet.ts';
 export { getFormattedDateTime } from './src/getFormattedDateTime.ts';
 
