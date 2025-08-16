0666415341423e64377c45d61bb4b748c73aa6d2
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-08-16 09:07:03 +0200

    version 0.1.18

---
+++ utils-nodejs/src/exec/escape.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prefer-template */
+
+// import { quote } from 'shell-quote/quote';
+
+export const escapeParamShell = (s: string): string => {
+    if (s === '') {
+        return '\'\'';
+    }
+    // if (s && typeof s === 'object') {
+    //     return s.op.replace(/(.)/g, '\\$1');
+    // }
+    if ((/["\s]/).test(s) && !(/'/).test(s)) {
+        return "'" + s.replace(/(['\\])/g, '\\$1') + "'";
+    }
+    if ((/["'\s]/).test(s)) {
+        return '"' + s.replace(/(["\\$`!])/g, '\\$1') + '"';
+    }
+    return String(s).replace(/([A-Za-z]:)?([#!"$&'()*,:;<=>?@[\\\]^`{|}])/g, '$1\\$2');
+}
---
+++ utils-nodejs/src/exec/shellEscape.ts
@@ -0,0 +1,29 @@
+import { escapeParamShell } from "./escape.ts";
+
+class ShellEscapedString {
+    protected nominal: 'nominal' = 'nominal' as const;
+    constructor(public readonly value: string) {}
+
+    toString() {
+        return this.value;
+    }
+}
+
+export const shellEscape = (
+  strings: TemplateStringsArray,
+  ...values: string[]
+): ShellEscapedString => {
+    const result: Array<string> = [];
+
+    strings.forEach((part, i) => {
+        result.push(part);
+
+        if (i < values.length) {
+            result.push(escapeParamShell(values[i]));
+        }
+    });
+
+    return new ShellEscapedString(result.join(''));
+};
+
+
