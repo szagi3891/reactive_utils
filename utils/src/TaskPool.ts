fd872ed848714cc85378e7a60ba4ea1e0da50cd9
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-11-15 20:39:32 +0100

    version 0.5.310

--- utils/src/TaskPool.ts
+++ utils/src/TaskPool.ts
@@ -1,4 +1,6 @@
-import { AsyncQuery, PromiseBox } from "@reactive/utils";
+import { AsyncQuery } from "./AsyncQuery.ts";
+import { PromiseBox } from "./PromiseBox.ts";
+import { timeout } from "./timeout.ts";
 
 const createTask = <RESOURCE, R>(task: (page: RESOURCE) => Promise<R>): [(page: RESOURCE) => Promise<void>, Promise<R>] => {
     const response = new PromiseBox<R>();
@@ -11,30 +13,48 @@ const createTask = <RESOURCE, R>(task: (page: RESOURCE) => Promise<R>): [(page: 
     return [taskBox, response.promise];
 };
 
+const execTask = async <RESOURCE>(
+    task: (page: RESOURCE) => Promise<void>,
+    resource: RESOURCE,
+    createResource: () => Promise<RESOURCE>
+): Promise<RESOURCE> => {
+    let currentResource: RESOURCE = resource;
+
+    while (true) {
+        try {
+            await task(currentResource);
+            return currentResource;
+        } catch {
+            currentResource = await createResource();
+            await timeout(5000);
+        }
+    }
+};
+
+const startWorker = async <RESOURCE>(
+    query: AsyncQuery<(page: RESOURCE) => Promise<void>>,
+    createResource: () => Promise<RESOURCE>
+) => {
+    let resource = await createResource();
+
+    for await (const task of query.subscribe()) {
+        resource = await execTask(task, resource, createResource);
+    }
+};
+
+
 export class TaskPool<RESOURCE> {
     private readonly query: AsyncQuery<(page: RESOURCE) => Promise<void>>;
 
-    constructor() {
+    constructor(size: number, createResource: () => Promise<RESOURCE>) {
         this.query = new AsyncQuery();
-    }
 
-
-    public addWorkerResource(resource: RESOURCE) {
-        (async () => {
-            const consumer = this.query.subscribe();
-
-            try {
-                for await (const task of consumer) {
-                    await task(resource);
-                }
-            } finally {
-                consumer.unsubscribe();
-            }
-        })().catch(console.error);
+        for (let i=1; i<=size; i++) {
+            startWorker(this.query, createResource).catch(console.error);
+        }
     }
 
     public execScenario<R>(scenario: (page: RESOURCE) => Promise<R>): Promise<R> {
-
         const [task, result] = createTask(scenario);
 
         this.query.push(task);
@@ -42,13 +62,9 @@ export class TaskPool<RESOURCE> {
     }
 
     public static async execScenarioList<R, P>(task: number, list: Array<P>, scenario: (param: P) => Promise<R>): Promise<Array<R>> {
-        const pool = new TaskPool();
+        const pool = new TaskPool(task, async () => null);
 
         try {
-            for (let i = 1; i <= task; i++) {
-                pool.addWorkerResource(undefined);
-            }
-
             const tasks: Array<Promise<R>> = [];
 
             for (const param of list) {
